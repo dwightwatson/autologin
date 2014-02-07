23ef0b110310af12ee36d3968c6b4bb41bf45b5c
Dwight Watson <dwightwatson@me.com>
2014-02-07 11:44:06 +1100

    Add unique index to the autologin token migration (#3)

---
+++ src/migrations/2014_02_07_004118_add_unique_index_to_autologin_tokens_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUniqueIndexToAutologinTokensTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('autologin_tokens', function(Blueprint $table)
+		{
+			$table->dropIndex('autologin_tokens_token_index')
+
+			$table->unique('token');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('autologin_tokens', function(Blueprint $table)
+		{
+			$table->dropUnique('tutors_user_id_unique');
+
+			$table->index('token');
+		});
+	}
+
+}
